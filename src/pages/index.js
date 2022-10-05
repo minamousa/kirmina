c15a8ce03872adb827d6baeffc34f7a1e6aab789
minamousa <50624392+minamousa@users.noreply.github.com>
2022-10-04 21:36:31 -0700

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -16,7 +16,7 @@ const Index = () => (
       <div className="container mx-auto px-8 lg:flex">
         <div className="text-center lg:text-left lg:w-1/2">
           <h1 className="text-4xl lg:text-5xl xl:text-6xl font-bold leading-none">
-            Main title of your landing page
+            Kirmina INC
           </h1>
           <p className="text-xl lg:text-2xl mt-6 font-light">
             Free landing page template to promote your business startup and generate leads for the
